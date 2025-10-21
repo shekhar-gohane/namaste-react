a9d9f79c2d1b16dd522d7620e87d9fa9e302724f
shekhar-gohane <shekharsgohane@gmail.com>
2025-10-21 12:27:29 +0530

    Add all files

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child1"},[
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-    ]),
-    React.createElement("div", {id:"child2"},[
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-    ]),
-])
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://seeklogo.com/images/F/foodx-online-food-ordering-system-logo-145CB16578-seeklogo.com.png" />
+            </div>
+            <div className="nav-item">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );  
+};
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <h3>Meghana Foods</h3>
+        </div>
+    );
+};
+
+const Body = () => {
+    <div className="body">
+        <div className="search">Search</div>
+        <div className="restaurant-container">
+            <RestaurantCard/>
+        </div>
+    </div>
+};
+
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout/>);
